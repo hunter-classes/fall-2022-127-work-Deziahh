a265d7207e497692e07e3805586c237b016e7db0
Deziah Gayle <81055368+Deziahh@users.noreply.github.com>
2022-08-31 18:25:40 -0400

    Update clock.py

--- chapter-2/clock.py
+++ chapter-2/clock.py
@@ -1,1 +1,10 @@
+timeNowString = input("What time is it now? (In Hours)")
+hoursToWaitString = input ("How many hours do you want to wait for the alarn to go off?")
 
+timeNowInt = int(timeNowString)
+hoursToWaitInt = int(hoursToWaitString)
+
+hours = timeNowInt + hoursToWaitInt
+
+timeOfDay = hours % 24
+print(timeOfDay)
