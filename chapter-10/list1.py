e292bd5d8287ea4d6e0102eea058c3fd3e7d07b3
Deziah Gayle <81055368+Deziahh@users.noreply.github.com>
2022-09-30 12:41:00 -0400

    Update list1.py

--- chapter-10/list1.py
+++ chapter-10/list1.py
@@ -1,1 +1,19 @@
+mylist = [76, 92.3, "hello", True, 4, 76]
+print(mylist)
 
+mylist.append("apple")
+mylist.append(76)
+
+mylist.insert(3, "cat")
+mylist.insert(0, 99)
+
+print(mylist)
+
+print(mylist.index("hello"))
+print(mylist.count(76))
+
+mylist.remove(76)
+
+mylist.pop(mylist.index(True))
+
+print(mylist)
