0280266645c41e03b583a0386a4059d13180d534
Deziah Gayle <81055368+Deziahh@users.noreply.github.com>
2022-09-30 18:08:09 -0400

    Update lists2.py

--- chapter-10/lists2.py
+++ chapter-10/lists2.py
@@ -1,1 +1,18 @@
+def average(numbers):
+  avg = 0
+  for num in numbers:
+      avg = avg + num
+  print(avg / len(numbers))
 
+#test:
+numbers = [4, 3, 3, 5]
+average(numbers)
+
+list = [2,3,4]
+def sum_of_squares(list):
+    sumSquares = 0
+    for number in list:
+        sumSquares += number ** 2
+    return sumSquares
+#test:
+print(sum_of_squares(list))
