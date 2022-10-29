66597b06f92b0bc9a2098ecf5ad1d8662f3deda1
Your Name <dezzz@Deziahs-MacBook-Air.local>
2022-10-29 12:39:28 -0400

    madlibs project

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,26 +1,30 @@
+
 import random
 
 names = ["Deziah", "Andrea", "Ally", "Sam"]
-schools = ["Hunter", "MHC", "CCNY", "Baruch"]
-majors = ["CompSci", "English", "Math"]
+rooms = ["living rooom", "bedroom", "dining room"]
+adjectives = ["silly", "enormous", "fun","fantastic"]
 verbs = ["destroyed", "earned", "accepted", "offer", "behaved"]
+adverbs = ["slowly", "yesterday", "today", "rarely"]
 nouns = ["dog", "hammer", "cat", "car"]
+animals = ["puppy", "kitten", "mouse"]
+food = ["pizza", "cheeseburgers", "salmon"]
 
 
 # Open the Mad Libs text file
 f = open("madlibs.txt")
  
 # Read the whole file and store each line in a list
-madlibText = f.readlines()
- 
-# Choose a random line from the list
-madlib = random.choice(madlibText)
+madlibText = f.read()
+madlibList = madlibText.split("\n")
 
+madlibList = [s.replace("blankName", random.choice(names)) for s in madlibList]
+madlibList = [s.replace("blankRoom", random.choice(rooms)) for s in madlibList]
+madlibList = [s.replace("blankAdjective", random.choice(adjectives)) for s in madlibList]
+madlibList = [s.replace("blankVerb", random.choice(verbs)) for s in madlibList]
+madlibList = [s.replace("blankAdverb", random.choice(adverbs)) for s in madlibList]
+madlibList = [s.replace("blankNoun", random.choice(nouns)) for s in madlibList]
+madlibList = [s.replace("blankAnimal", random.choice(animals)) for s in madlibList]
+madlibList = [s.replace("blankFood", random.choice(food)) for s in madlibList]
 
-madlib = madlib.replace("blankName", random.choice(names))
-madlib = madlib.replace("blankSchool", random.choice(schools))
-madlib = madlib.replace("blankMajor", random.choice(majors))
- 
-# Print out the Mad Lib including the user's response
-print(madlib)
-
+print(madlibList)
