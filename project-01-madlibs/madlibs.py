46b45920773708cff5964bafc696c4adb0f206ab
Your Name <dezzz@Deziahs-MacBook-Air.local>
2022-10-30 20:04:45 -0400

    madlibs project

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,3 +1,5 @@
+#Extra 1: I wrote a story in another file and read it in my program
+#Extra 2: Unique replacement (heroName)
 
 import random
 
@@ -10,6 +12,8 @@ nouns = ["dog", "hammer", "cat", "car"]
 animals = ["puppy", "kitten", "mouse"]
 food = ["pizza", "cheeseburgers", "salmon"]
 
+randomName = random.choice(names)
+names.remove(randomName)
 
 # Open the Mad Libs text file
 f = open("madlibs.txt")
@@ -18,13 +22,18 @@ f = open("madlibs.txt")
 madlibText = f.read()
 madlibList = madlibText.split("\n")
 
-madlibList = [s.replace("blankName", random.choice(names)) for s in madlibList]
-madlibList = [s.replace("blankRoom", random.choice(rooms)) for s in madlibList]
-madlibList = [s.replace("blankAdjective", random.choice(adjectives)) for s in madlibList]
-madlibList = [s.replace("blankVerb", random.choice(verbs)) for s in madlibList]
-madlibList = [s.replace("blankAdverb", random.choice(adverbs)) for s in madlibList]
-madlibList = [s.replace("blankNoun", random.choice(nouns)) for s in madlibList]
-madlibList = [s.replace("blankAnimal", random.choice(animals)) for s in madlibList]
-madlibList = [s.replace("blankFood", random.choice(food)) for s in madlibList]
+
+madlibList = [sentence.replace("heroName", randomName) for sentence in madlibList]
+
+madlibList = [sentence.replace("blankName", random.choice(names)) for sentence in madlibList]
+madlibList = [sentence.replace("blankRoom", random.choice(rooms)) for sentence in madlibList]
+madlibList = [sentence.replace("blankAdjective", random.choice(adjectives)) for sentence in madlibList]
+madlibList = [sentence.replace("blankVerb", random.choice(verbs)) for sentence in madlibList]
+madlibList = [sentence.replace("blankAdverb", random.choice(adverbs)) for sentence in madlibList]
+madlibList = [sentence.replace("blankNoun", random.choice(nouns)) for sentence in madlibList]
+madlibList = [sentence.replace("blankAnimal", random.choice(animals)) for sentence in madlibList]
+madlibList = [sentence.replace("blankFood", random.choice(food)) for sentence in madlibList]
 
 print(madlibList)
+
+f.close()
