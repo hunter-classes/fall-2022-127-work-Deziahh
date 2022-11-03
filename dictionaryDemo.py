fc76e74ba85cb4e41ba6baeb63dc9cbe17d781fe
deziahh <deziahgayle@gmail.com>
2022-11-03 09:37:30 -0400

    dictionary demo code

---
+++ dictionaryDemo.py
@@ -0,0 +1,11 @@
+d = {} #empty dictionary
+d[2] = 12345
+d[5] = "hello"
+
+#can use strings as keys
+d["hello"] = "world"
+d["list"] = [2,3,4,4,6]
+d["list"] = 55.3
+
+person = {"first" : "John", "last" : "Smith", "age" : 50}
+person["age"] = person["age"] + 1
