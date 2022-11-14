a589dbeb7d10df56818fa35508ee4ac04f636ba3
deziahh <deziahgayle@gmail.com>
2022-11-14 13:53:39 -0500

    did basic project

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -1,1 +1,19 @@
+pirateWords = {"Hello":"Ahoy",
+               "buddy":"matey"}
 
+pirateKeys = list(pirateWords.keys())
+pirateValues = list(pirateWords.values())
+
+
+file = open("input.txt")
+fileData = file.read()
+fileLines = fileData.split("\n")
+
+#print(list(pirateWords.keys())[0])
+
+fileLines = [line.replace(pirateKeys[0], pirateValues[0]) for line in fileLines]
+fileLines = [line.replace(pirateKeys[1], pirateValues[1]) for line in fileLines]
+
+
+done = " ".join(fileLines)
+print(done)
